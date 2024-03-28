5b438b3ad39d217da10971d9c22da86a5cd9ddd1
UPDT-Lucas <122400237+UPDT-Lucas@users.noreply.github.com>
2024-03-27 19:16:39 -0600

    badge
    -badge component

--- front/src/app/shared/components/table/table.component.ts
+++ front/src/app/shared/components/table/table.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { BadgeComponent } from '../badge/badge.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [
+    BadgeComponent
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
