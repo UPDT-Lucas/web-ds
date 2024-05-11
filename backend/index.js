4ba3d48615ee1c1a4a433a73c4ddc8ad7a8fa7b0
David Centeno Araya <100227276+davidCenteno999@users.noreply.github.com>
2024-05-11 13:45:43 -0600

    patron singleton

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,29 @@
-require('dotenv').config()
-const express = require('express');
-const {appConfig, dbConfig} = require('./config');
-const connectDb = require('./db/mongodb')
+require('dotenv').config(); 
+const { appConfig, dbConfig } = require('./config');
+const app = require('./app');
+const MongoConnection = require('./db/class_db'); 
+const Student = require('./Models/Student');
 
+async function initApp(appConfig, dbConfig) {
+    try {
 
-const app = require('./app')
+        const mongoConnection = new MongoConnection({
+            user: "proyecto_diseno",
+            password: "MLdmnzq3DcLU7Jqw",
+            address: "cluster0.cj7hwee.mongodb.net",
+            nameDB: "proyecto_diseno"
+        });
+        
 
-async function initApp (appConfig, dbConfig) {
-    try{
-        await connectDb(dbConfig)
-        app.listen(appConfig.port, () => console.log( `listening on http://localhost:${appConfig.port} `))
-    } catch (e){
-        console.error(e)
-        process.exit(0)
-    }   
+        await mongoConnection.connect();
+
+      
+
+        app.listen(appConfig.port, () => console.log(`Listening on http://localhost:${appConfig.port}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(0);
+    }
 }
 
-initApp(appConfig, dbConfig)
+initApp(appConfig, dbConfig);
