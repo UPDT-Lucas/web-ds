3207cb732d5f95571062d2a5c276041cc804d0d6
Arianaam335 <arianaam335@estudiantec.cr>
2024-05-16 01:00:35 -0600

    validation

--- front/src/app/access/pages/change-password-page/change-password-page.component.ts
+++ front/src/app/access/pages/change-password-page/change-password-page.component.ts
@@ -3,13 +3,17 @@ import { InputComponent } from '../../../shared/components/input/input.component
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { CommunicationService } from '../../../services/communication.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-change-password-page',
   standalone: true,
   imports: [
     InputComponent,
-    ButtonComponent
+    ButtonComponent,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './change-password-page.component.html',
   styleUrl: './change-password-page.component.css'
@@ -24,11 +28,34 @@ export class ChangePasswordPageComponent {
     this.id = localStorage.getItem('-id') || '';
   }
 
+  showErrorMessage: boolean = false;
+  errorMessage: string = '';
+
 
   getInput() {
   
     if (this.id) {
       console.log(this.id)
+
+      
+    if (!this.newPassword && !this.confirmPassword) {
+      this.errorMessage = 'Por favor ingrese tanto el contraseña nueva como la contraseña de confirmación.';
+      this.showErrorMessage = true; 
+      return;
+    }else if (!this.newPassword) {
+      this.errorMessage = 'Por favor ingrese la contraseña nueva';
+      this.showErrorMessage = true; 
+      return; 
+    } else if (!this.confirmPassword) {
+      this.errorMessage = 'Por favor ingrese la contraseña de confirmación.';
+      this.showErrorMessage = true; 
+      return; 
+    } else if (!this.isValidPassword(this.newPassword, this.confirmPassword)) {
+      this.errorMessage = 'Por favor ingrese una contraseña con mínimo 8 dígitos.';
+      this.showErrorMessage = true; 
+      return; 
+    }
+
       this.CS.resetPassword(this.id, this.newPassword, this.confirmPassword).subscribe(
         (res) => {
           console.log(res);
@@ -48,4 +75,9 @@ export class ChangePasswordPageComponent {
       console.error("ID de usuario no disponible");
     }
   }
-}
+
+  isValidPassword(newPassword: string, confirmationPassword: string): boolean {
+    return newPassword.length >= 8 && newPassword === confirmationPassword;
+  }
+}  
+
